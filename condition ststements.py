64dffd75f91b3382600d2b76e1a6c20b993b3a7a
Reshma Palivela <reshmasaipalivela@gmail.com>
2025-07-12 12:52:53 +0530

    Add files via upload

---
+++ condition ststements.py
@@ -0,0 +1,50 @@
+
+'''if'''
+# a = 5
+# if a==5:
+#     print("the if condition is executed...")
+
+'''if else'''
+
+# a = 10
+# b = 15
+ 
+# if a==b and b>a: # F and T --F
+#     print("the if block is executed...")
+# else:
+#     print("the else block is executed...")
+
+
+'''elif'''
+
+# name = input("enter a name: ")
+# userid = int(input("enter the userid.."))
+# password = input("enter a password... ")
+
+# if userid == 12345 and password=="ists@123":
+#     print("the if block is executed")
+
+# elif userid== 456789 and password=="ists@456":
+#     print("the elif block is executed")
+# else:
+#     print("the else block is executed")
+
+'''nested if'''
+
+# a = 29
+# b = 46
+# c = 56
+# d = 49
+# if a==20 and b==46:
+#     print("the outer if is executed")
+#     if c==56:
+#         print("the inner if is executed")
+#     else:
+#         print("the inner else is executed")
+# else:
+#     print("the outer else is executed")
+
+'''shorthandif'''
+
+# a= 5
+# if a==5: print("the if condition is executed")
