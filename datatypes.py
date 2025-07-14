33191d81a98d738baff46fda7be0c73611e60541
Reshma Palivela <reshmasaipalivela@gmail.com>
2025-07-14 20:14:51 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,83 @@
+"Number Datypes"
+
+# a = 123
+# print(a)
+# print(type(a))
+# print(id(a))
+
+# a1 = -134
+# print(a1)
+# print(type(a1))
+# print(id(a1))
+
+# b = 23.345
+# print(b)
+# print(type(b))
+# print(id(b))
+
+# b1 = -34.567
+# print(b1)
+# print(type(b1))
+# print(id(b1))
+
+# c = 23+56j
+# print(c)
+# print(type(c))
+# print(id(c))
+
+# c1= -23+567j
+# print(c1)
+# print(type(c1))
+# print(id(c1))
+
+
+"sequence datatypes"
+
+# a = "cse"
+# print(a)
+# print(type(a)) 
+# print(id(a))
+
+
+'''list'''
+
+# mylist = [12,23.56, 45+67j, "cse",[1,2,3],{12,34,56},(23,45,"cse")] #items/values/elements=5
+# print(mylist)
+# print(type(mylist))
+# print(mylist[0])
+# print(mylist[3])
+# print(mylist[4])
+
+# mylist[4]="dept"
+# print(mylist)
+
+
+
+'''Tuple''' 
+# mytuple=(12,23.45,56+78j,"cse",(1,23,34),[12,34,56])
+# print(mytuple)
+# print(type(mytuple))
+# print(mytuple[0])
+# print(mytuple[2])
+# print(mytuple[5])
+
+
+"Boolean Datatypes"
+# a = True
+# print(type(a))
+# b = False
+# print(type(b))
+
+# "set"
+# myset= {12,34,45,56.89, 45+67j, "cse",(12,23,45),True}
+# print(myset)
+# print(type(myset))
+
+"Dictionary"
+
+# mydictionary = {1:"cse", 2:2345,"dept":"cse"}
+# print(mydictionary)
+# print(type(mydictionary))
+# mydictionary[1]="ml"
+# print(mydictionary)
+
