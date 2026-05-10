699c9ac2df4c516a967d34b693e42ee73f01471d
Reshma Palivela <reshmasaipalivela@gmail.com>
2026-05-10 17:25:46 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,126 @@
+# print("hello world!")
+'''type conversion'''
+# a=100
+# b=1.5
+# c=a+b
+# print(type(b))
+# print(type(c)
+''' types'''
+# a=100
+# a=(float(a))
+# print(a)
+# print(type(a))
+# a=100.0
+# a=(double(a))
+# print(a)
+# print(type(a))
+'''bulit  in functions'''
+# num=5+2.0
+# print(int(num))
+# print(num)s
+# num_int=((5.7))
+# print(type(num_int))
+# print(bool(num_int))
+# a=100
+# print(type(a))
+# print(float(a))
+# is_true=bool(1)
+# is_flase=bool(0)
+# sss="j0hn"
+# sss=int(sss)
+# print(sss)
+# a="1"
+# a=tuple(a)
+# print(a)
+
+'''ASCII values'''
+# char_a="A"
+# b=ord(char_a)
+# print(b)
+# a="Z"
+# c=ord(a)
+# print(c)
+# a=90
+# b=chr(a)
+# print(b)
+# b=100
+# c=chr(b)
+# print(c)
+'''operators'''
+
+'''arithmetic operator(+,-,*,/)'''
+# a=5
+# b=10
+# c=a+b
+# print("Addition:",c)
+# a=5
+# b=10
+# c=a*b
+# print("multiplication:",c)
+# a=5
+# b=10
+# c=a%b
+# print("multiplication:",c)
+'''assignment operator(=,+=,-=,*=,/=)'''
+# a=10
+# b=20
+# a=a-b
+# print("A:",a)
+# a=10
+# b=20
+# a*=b
+# print("A:",a)
+'''comparision operator(==,<=,!=)'''
+# a=5
+# b=10
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# c=b-a
+# c=c>=a
+# print(c)
+''' logical opetrator(and,or,not)'''
+# a=0
+# b=1
+# c=a and b
+# print(c)
+# a=0
+# b=1
+# c=a  or b
+# print(c)
+# a=0
+# b=1
+# c=not b
+# print(c)
+'''membership operator(in,not in)'''
+# t=[1,2,3,4]
+# print(1 in t)
+# print(10 not in t)
+# fruits=["apple","orange","mango",10]
+# c="apple" not in fruits
+# print("fruits:",c)
+# print(10 in fruits)
+''' identity operator(is,is not)'''
+# name1="jhon"
+# name2="jhon"#=1
+# c=name1 is not name2 
+# print(c)
+# print( name1 is name2)
+''' bitiwise operator(&,|,~,^)'''
+# a=2
+# b=3
+# c=a|b
+# print(c)
+
+# a=2
+# b=2
+# c=a>>b
+# print(c)
+# a=2
+# b=3
+# c=a&b
+# print(c)
+
+
+
