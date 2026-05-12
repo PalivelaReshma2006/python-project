049025caaeb7bf314acdc0ed56aee183d1fb6d2c
Reshma Palivela <reshmasaipalivela@gmail.com>
2026-05-12 17:53:16 +0530

    Add files via upload

---
+++ day 3.py
@@ -0,0 +1,92 @@
+'''examples'''
+'''sum of two numbers'''
+# a=int(input("num1="))
+# b=int(input("num2="))
+# sum=a+b
+# print("Sum:",sum,sep="")
+# print(f"Sum:{a+b}")
+'''(num1=5
+num2=10
+Sum:15
+Sum:15)'''
+'''radius of the circle'''
+# radius=int(input("enter radius:"))#3.14(pie)r^2 /r*r
+# a= 3.14*(radius*radius)
+# print("Area of the circle:",a,sep="")
+# print(f"Area of the circle:{a}")
+'''(enter radius:5
+Area of the circle:78.5
+Area of the circle:78.5)'''
+'''roots(quadratic equations)'''
+# a=int(input("a="))
+# b=int(input("b="))
+# c=int(input("c="))
+# root1=0
+# root2=0
+# d=(b**2)-4*a*c
+# root1=(-b+(d**(0.5)))/2*a
+# root2=(-b-(d**(0.5)))/2*a
+# print("Roots:",(root1,root2),sep="")
+# print(f"Roots:{root1,root2}")
+'''(a=1
+b=-3
+c=2
+Roots:(2.0, 1.0)
+Roots:(2.0, 1.0))'''
+'''swap the values of two variabiles without using temporay variables'''
+# a=int(input("a="))
+# b=int(input("b="))
+# b=b+a
+# a=b-a
+# b=b-a
+# print("after swaping:")
+# print("a=",a)
+# print("b=",b)
+# print(f"a={a}")
+# print(f"b={b}")
+'''(a=10
+b=20
+after swaping:
+a= 20
+b= 10
+a=20
+b=10)'''
+'''with temp variable'''
+# a=int(input("a="))
+# b=int(input("b="))
+# temp=a
+# a=b
+# b=temp
+# print("after swaping:")
+# print("a=",a)
+# print("b=",b)
+# print(f"a={a}")
+# print(f"b={b}")
+'''(a=10
+b=20
+after swaping:
+a= 20
+b= 10
+a=20
+b=10)'''
+'''(converting  temperature units)''' 
+# c=int(input("temperature_celsius="))
+# f=c*(9/5)+32
+# k=273+c
+# print("Temperatur in faharenheit:",f)
+# print("Temperatur in kelvin:",k)
+# print(f"Temperatur in faharenheit:{f}")
+# print(f"Temperatur in kelvin:{k}")
+'''(temperature_celsius=30
+Temperatur in faharenheit: 86.0
+Temperatur in kelvin: 303
+Temperatur in faharenheit:86.0
+Temperatur in kelvin:303)'''
+'''currency converter'''
+# amo=int(input("amount_in_used:"))
+# exc=float(input("exchange_rate_usd_to_eur="))
+# exchange=exc*amo
+# print("equivalent amount in EUR:",exchange,sep="")
+'''(amount_in_used:100
+exchange_rate_usd_to_eur=0.85
+equivalent amount in EUR:85.0)'''
