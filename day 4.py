827785ad3395af5cda2ef4cf5ab32fc5d1cbe881
Reshma Palivela <reshmasaipalivela@gmail.com>
2026-05-13 16:38:09 +0530

    Add files via upload

---
+++ day 4.py
@@ -0,0 +1,83 @@
+'''decision making'''
+'''condition statements'''
+'''IF statement'''
+# weather=input("give weather:")
+# if weather=="sunny":
+#     print("circket")
+'''else statement'''
+# else:
+#     print("robot game")
+# print("code end here!!!")
+'''(sunny
+circket
+code end here!!!)'''
+'''if-elif-else'''
+# weather=input("give weather:")
+# if weather=="sunny":
+#     print("circket")
+# elif weather=="rainy":
+#     print("play indoor games!")
+#     '''else statement'''
+# else:
+#     print("robot game")
+# print("code end here!!!")
+'''(give weather:rainy
+play indoor games!
+code end here!!!)'''
+'''EXAMPLE'''
+# weather=input("give weather:")
+# day=input("enter day:")
+# if weather=="sunny":
+#     print("circket")
+# elif weather=="rainy":
+#     print("play indoor games!")
+# elif weather=="snowy" and day=="night":
+#     print(" sleeping")
+# else:
+#     print("robot game")
+# print("code end here!!!")
+'''give weather:snowy
+enter day:night
+ seelping
+code end here!!!'''
+'''Nested if statement'''
+# weather=input("give weather:")
+# day=input("enter day:")
+# if weather=="sunny":
+#     if day=="afternoon":
+#         print("eat")
+#     else:
+#         print("not eat")
+#     print("circket")
+# elif weather=="rainy":
+#     print("play indoor games!")
+# elif weather=="snowy":
+#     if day=="night":
+#         print("sleeping")
+#     else:
+#         print("not sleep")
+# else:
+#     print("robot game")
+# print("code end here!!!")
+'''example'''
+
+# num1=int(input("enter num1:"))
+# num2=int(input("enter num2:"))
+# operation=input("enter operation:")
+# # operations="addition","subtraction","multiplication","division"
+# if operation=="addition":
+#     print(num1+num2)
+# elif operation=="subtraction":
+#     print(num1-num2)
+# elif operation=="multiplication":
+#     print("multiplication:",num1*num2)
+# elif operation=="division":
+#     print("division:",num1/num2)
+# else:
+#     print("not valid")
+# print("operation completed!!")
+'''(enter num1:10
+enter num2:5
+enter operation:division
+division: 2.0
+operation completed!!)'''
