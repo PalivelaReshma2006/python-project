34cc106a4d2326b97442bef5cd73dead4e707c82
Reshma Palivela <reshmasaipalivela@gmail.com>
2026-05-16 16:33:49 +0530

    Add files via upload

---
+++ day 7.py
@@ -0,0 +1,64 @@
+'''Examples'''
+'''finding the sum of two numbers'''
+# a=int(input("enter:"))
+# b=int(input("enter:"))
+# def add2numbers(a,b):
+#     print(a+b)
+# add2numbers(a,b)
+'''enter:10
+enter:5
+15'''
+'''finding the area of a circle'''
+# def area(r):
+#     circleArea= 3.14*r*r
+#     return circleArea
+# radius=1
+# a=area(radius)
+# print(" circle area:",a)
+'''(circle area: 3.14)'''
+'''quadratic equation'''
+# def calculateroots(a,b,c):
+#     root1=0
+#     root2=0
+#     d=(b**2)-4*a*c
+#     root1=(-b+(d**(0.5)))/2*a
+#     root1=(-b-(d**(0.5)))/2*a
+#     print(f"roots:{root1,root2}")
+# x=int(input("give a:"))
+# y=int(input("give b:"))
+# z=int(input("give c:"))
+# calculateroots(y,x,z)
+'''(give a:1
+give b:-3
+give c:2
+roots:(1.0, 0))'''
+'''swap the values of two variables without using a temporary variable'''
+# a=int(input("enter a:"))
+# b=int(input("enter b:"))
+# def  swap(a,b):
+#     b=b+a
+#     a=b-a
+#     b=b-a
+#     print("after swaping:")
+#     print("a=",a)
+#     print("b=",b)
+# swap(20,30)
+'''enter a:10
+enter b:20
+after swaping:
+a= 30
+b= 20'''
+'''converting tempertarue units'''
+# c=int(input("Temperatur_celsius:"))
+# f=c*(9/5)+32
+# k=273+c
+# def temp(f,k):
+#     print("temperature of f:",f)
+#     print("temperature of f:",k)
+# temp(f,k)
+'''Temperatur_celsius:30
+temperature of f: 86.0
+temperature of f: 303'''
+
+
+
