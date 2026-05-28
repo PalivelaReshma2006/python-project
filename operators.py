71cb299032db6ebab10dcfc4007275ea2e3aa96a
Reshma Palivela <reshmasaipalivela@gmail.com>
2026-05-28 17:43:24 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,97 @@
+'''Write a program if a person is eligible to vote or not in'''
+# n = int(input("enter a number: "))
+# if n>=18 :
+#  print("the person is eligible to vote....")
+
+
+'''write a Program to find number is divisible by 3 and 5'''
+
+# n = int(input("enter a number..."))
+# if n%3==0 and n%5==0 :
+#  print("the number is divisible by 3 and 5.....")
+
+
+'''Write a program to find number is even'''
+# n = int(input("enter a number...."))
+# if n%2==0:
+#  print("the num is even....")
+
+'''Write a program to find an odd number '''
+ 
+# n = int(input("enter a number...."))
+# if n%2==1:
+#  print("the num is odd....")
+
+'''Write a program to find biggest of two numbers '''
+# a = int(input("enter a number::"))
+# b = int(input("enter a number::"))
+# if a>b and b<a:
+#  print("a is bigger..")
+
+'''identity opreator'''
+
+# x = [ 1,2,3]
+# y = [4,5,6]
+# z =x 
+# print(x is z)#True
+# print(x is y)#False
+# print(x is not y)#True
+# print(z is not y)#True
+
+'''logical operator'''
+
+# a = 23
+# b = 30
+# c = a<b and b>a #t and t -- t
+# print(c)#True
+
+# d = 40
+# e = 45
+# f = d>e or e>d # f or t --t 
+# print(f)#true
+
+# a = 1
+# print(not(a)) #FALSE 
+
+# a = 0
+# print(not(a)) #True
+
+'''membership operator'''
+
+# x = ["apple", "banana", "jack", "kiwi"]
+# print("pineapple" in x) #False
+# print("apple" in x)#True
+# print("dragon" not in x)#True
+# print("banana" not in x)#False
+
+
+'''relational operator'''
+
+# a = int(input("enter a number: "))
+# b = int(input("enter a number: "))
+# print("The greater value", a>b)
+# print("The lesser value", a<b)
+# print("The greater than and equals too value",a>=b)
+# print("The lesse than and equals too value",a<=b)
+# print("the equals too value",a==b)
+# print("the not equals too value",a!=b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
